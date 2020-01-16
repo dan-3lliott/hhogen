7187d5d61a310f8a9704c5683e0ba611441d8720
Daniel Elliott <elliott.daniel.ballantyne@gmail.com>
2020-01-16 10:35:19 -0700

    Working on rate vs voltage graph

--- R code/ratevsvolt.R
+++ R code/ratevsvolt.R
@@ -5,6 +5,6 @@ rates <- c(2.4, 12, 35.6, 73.2)
 
 dframe <- data.frame(voltage, rates)
 
-plot(dframe, col = "dodgerblue")
+plot(dframe, col = "dodgerblue", type = "l", xlab = "Voltage (V)", ylab = "Rate of Oxyhydrogen Gas Production (ml/min)", pch = 19, main = "Input Voltage vs Rate of Gas Production")
 
 dev.off()
