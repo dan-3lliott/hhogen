a24b74c6326416bd530515b86d5b1084e739368b
Daniel Elliott <elliott.daniel.ballantyne@gmail.com>
2020-03-07 16:02:43 -0700

    Updated current and regressions

--- R code/ratevscurrent.R
+++ R code/ratevscurrent.R
@@ -1,7 +1,7 @@
 png(file = "ratevscurrent.png")
 
-current <- c(1.25, 2.5, 3.75, 5)
-rates <- c(2.4, 12, 35.6, 73.2)
+current <- c(4, 8, 12, 16, 20)
+rates <- c(1.73, 2.44, 2.88, 3.11, 3.29)
 
 plot(current, rates, col = "dodgerblue", type = "l",lwd = 2.5, xlab = "Current (A)", ylab = "Rate of Oxyhydrogen Gas Production (ml/min)", main = "Current vs Rate of Gas Production")
 points(current, rates, pch = 19, col = "dodgerblue")
--- R code/ratevscurrentregression.R
+++ R code/ratevscurrentregression.R
@@ -1,17 +1,19 @@
-current <- c(1.25, 2.5, 3.75, 5)
-rates <- c(2.4, 12, 35.6, 73.2)
+current <- c(4, 8, 12, 16, 20)
+rates <- c(1.73, 2.44, 2.88, 3.11, 3.29)
 
 linreg <- lm(rates~current)
 expreg <- lm(log(rates)~current)
 pwrreg <- lm(log(rates)~log(current))
+logreg <- lm(rates~log(current))
 
 print(summary(linreg))
 print(summary(expreg))
 print(summary(pwrreg))
+print(summary(logreg))
 
 png(file = "ratevscurrentregression.png")
 
-plot(log(current), log(rates), col = "dodgerblue", pch = 19, xlab = "log(Current) (A)", ylab = "log(Rate of Oxyhydrogen Gas Production) (ml/min)", main = "Current vs Rate of Gas Production (Power Regression)")
-abline(pwrreg, col = "firebrick1", lwd = 2.5)
+plot(log(current), rates, col = "dodgerblue", pch = 19, xlab = "log(Current) (A)", ylab = "log(Rate of Oxyhydrogen Gas Production) (ml/min)", main = "Current vs Rate of Gas Production (Power Regression)")
+abline(logreg, col = "firebrick1", lwd = 2.5)
 
 dev.off()
