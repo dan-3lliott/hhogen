0aa199d17242a1473df7fda7ad97c958f8100714
Daniel Elliott <elliott.daniel.ballantyne@gmail.com>
2020-03-07 16:49:24 -0700

    Updated everything with new data

--- R code/ratevscurrentregression.R
+++ R code/ratevscurrentregression.R
@@ -13,7 +13,8 @@ print(summary(logreg))
 
 png(file = "ratevscurrentregression.png")
 
-plot(log(current), rates, col = "dodgerblue", pch = 19, xlab = "log(Current) (A)", ylab = "log(Rate of Oxyhydrogen Gas Production) (ml/min)", main = "Current vs Rate of Gas Production (Power Regression)")
+plot(log(current), rates, col = "dodgerblue", pch = 19, xlab = "log(Current) (A)", ylab = "Rate of Oxyhydrogen Gas Production (ml/min)", main = "Current vs Rate of Gas Production (Logarithmic Regression)", yaxt = "n")
+axis(2, seq(1.75, 3.25, by = 0.25))
 abline(logreg, col = "firebrick1", lwd = 2.5)
 
 dev.off()
